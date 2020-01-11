4e3043d5df184a0e9f1fb79e98ab5cfdea254287
TR-SLimey <tr_slimey@protonmail.com>
2020-01-11 19:21:21 +0000

    Basic GUI framework is now working The client can now switch between any window layout easily and the window layouts can be (relatively) easily edited. The client does not yet support any communication with the server.

--- src/client/LT-client.py
+++ src/client/LT-client.py
@@ -45,56 +45,63 @@ def log(level, message):
 class Client():
     """The main class of the LanTalk client which handles everything."""
 
-    # Init class properties
-    master = tk.Tk() # The main window
-    current_widgets = [] # A list of all the widgets currently displayed
-
-    # Define the different windows and what properties they have
-    windows = {
-        "DEFAULT": {
-            "title": "LanTalk Client",
-            "size": "500x500",
-            "minsize": "500x500",
-            "maxsize": "600x600",
-            "widgets": { # 2D list of widgets. Sub lists contain: [widget_object, placing_method], for example: [lambda widget: widget.grid(row=1, column=1), tk.Button(text="Example")]
-
-            },
-        },
-        "SERVER_FINDER": {
-            "title": "LanTalk Client - Server Finder",
-            "size": "500x500",
-            "minsize": "500x500",
-            "maxsize": "600x600",
-            "widgets": [
-                [tk.Label(master, text="LanTalk Client", background="#444444", height=5), lambda w: w.grid(row=0, column=0, columnspan=4, sticky=tk.W+tk.E)],
-                [tk.Frame(master, height="200", width="500"), lambda w: w.grid(row=1, column=0, columnspan=4, sticky=tk.W+tk.E)]
-            ]
-        }
-    }
-
     def __init__(self):
-        self.createwindow("SERVER_FINDER")
+        # Init class properties
+        self.master = tk.Tk() # The main window
+        self.current_widgets = {} # A dict of all the widgets currently displayed with names as keys
+
+        # Define the different windows and what properties they have
+        self.windows = {
+            "DEFAULT": {
+                "title": "LanTalk Client",
+                "size": "500x500",
+                "minsize": "500x500",
+                "maxsize": "600x600",
+                "widgets": { # 2D dict of widgets. Lists contain: [widget_object_creator, placing_method], for example: [lambda: tk.Button(text="Example"), lambda widget: widget.grid(row=1, column=1)]. The lambda is nescessary before the widget object so that a new object is created every time, otherwise there are errors
+                },
+            },
+            "SERVER_FINDER": {
+                "title": "LanTalk Client - Server Finder",
+                "size": "500x500",
+                "minsize": "500x500",
+                "maxsize": "600x600",
+                "widgets": {
+                    "title_label": [lambda: tk.Label(self.master, text="LanTalk Client", background="#444444"), lambda w: w.grid(row=0, column=0, columnspan=4, sticky=tk.N+tk.S+tk.W+tk.E)],
+                    "server_list_box": [lambda: tk.Frame(self.master, background="#444444"), lambda w: w.grid(row=1, column=0, columnspan=4, sticky=tk.N+tk.S+tk.W+tk.E)]
+                }
+            }
+        }
+
+        self.master.columnconfigure(1, weight=1) # Automatically resize everything when window size changes
+
+        self.createwindow("SERVER_FINDER") # Open straight to the server finder
 
     def createwindow(self, name):
         """Clears all the widgets on the window and replaces them with the widgets of the specified window."""
-        self.resetwindow() # Reset the window first of all
-        window = self.windows[name]
-        self.master.title(window["title"])
-        self.master.geometry(window["size"])
-        for widget in window["widgets"]:
-            widget[1](widget[0])
 
+        self.resetwindow() # Reset the window first of all
+        window = self.windows[name] # Select the given window
+        self.master.title(window["title"]) # Set the title
+        self.master.geometry(window["size"]) # Set the size of the window
+        for widget_name in window["widgets"].keys(): # For every widget name
+            self.current_widgets[widget_name] = window["widgets"][widget_name][0]() # Add the widget to the list of widgets
+            window["widgets"][widget_name][1](self.current_widgets[widget_name]) # Put the grid on the window
 
     def resetwindow(self):
-        """Removes all widgets from the window"""
-        for widget in self.current_widgets:
-            try: widget.destroy()
+        """Removes all widgets from the window and resets everything to default."""
+
+        self.master.title(self.windows["DEFAULT"]["title"]) # Reset title to default
+        self.master.geometry(self.windows["DEFAULT"]["size"]) # Reset size to default
+        for widget_name in self.current_widgets.copy().keys(): # For every currently existing widget
+            try: self.current_widgets[widget_name].destroy() # Try removing the widget but fail silently (it could have been a child of a previously removed widget)
             except: pass
+            del self.current_widgets[widget_name] # Remove the widget from the list of existing widgets
 
     def mainloop(self):
         """Runs the mainloop of the main window."""
+
         self.master.mainloop()
-        print(1)
+        print("Exited GUI") # Temporary
 
 #
 # Main body
