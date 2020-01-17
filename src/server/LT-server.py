01f0d037e136713bec9cbe9b013afffe86ffdf34
TR-SLimey <tr_slimey@protonmail.com>
2020-01-17 22:01:04 +0000

    Final PEP8 compliance updates (LT Server)

--- src/server/LT-server.py
+++ src/server/LT-server.py
@@ -149,38 +149,56 @@ def conf_parse(conf_string):
             continue
         # Split the line at the first = sign
         line = line.split("=", 1)
-        if len(line) != 2: raise InvalidConfigException("File: `{}`, line: `{} /{}`, setting: `{}`. Invalid setting.".format(CONF_LOCATION, current_line, len(lines),line[0])) # If the line is not the right format, throw an error
-        config[line[0].strip()] = line[1].strip() # Add setting to config. Extra strip needed in case there are spaces around = sign
-    return config # Return the parsed config as dict
+        if len(line) != 2:
+            # If the line is not the right format, throw an error
+            raise InvalidConfigException("File: `{}`, line: `{} /{}`, setting: `{}`. Invalid setting.".format(CONF_LOCATION, current_line, len(lines),line[0]))
+            # Add setting to config. Extra strip needed in case there
+            # are spaces around = sign
+        config[line[0].strip()] = line[1].strip()
+    # Return the parsed config as dict
+    return config
+
 
 def conf_validate(conf_dict):
-    """Validates the config for the server making sure every option is valid."""
+    """Validate the config for the server making sure every option is valid."""
     for setting in conf_dict.keys():
-        # If the setting isn't on the pre-defined list, it's invalid so throw an error
-        if setting not in VALID_CONF_OPTIONS.keys(): raise InvalidConfigException("File: `{}`. Invalid setting: `{}`.".format(CONF_LOCATION, setting))
+        # If the setting isn't on the pre-defined list, it's invalid
+        # so throw an error
+        if setting not in VALID_CONF_OPTIONS.keys():
+            raise InvalidConfigException("File: `{}`. Invalid setting: `{}`.".format(CONF_LOCATION, setting))
         # Run the testing function for the setting
         if not VALID_CONF_OPTIONS[setting](conf_dict[setting]):
             raise InvalidConfigException("Invalid value `{}` for setting `{}` in config file `{}`!".format(conf_dict[setting], setting, CONF_LOCATION))
-        return conf_dict # Despite being unchanged, return the dict for one-liners
+            # Despite being unchanged, return the dict for one-liners
+        return conf_dict
+
 
 def conf_add_missing(conf_dict):
-    """Adds missing settings to the config and gives them default values."""
+    """Add missing settings to the config and gives them default values."""
     for setting in DEFAULT_CONF_OPTIONS.keys():
         # If the setting isn't in the user config
-        if not setting in conf_dict.keys():
+        if setting not in conf_dict.keys():
             # Add it with the pre-defined default value
             conf_dict[setting] = DEFAULT_CONF_OPTIONS[setting]
             log(0, "Added setting `{} = {}` (default option)".format(setting, conf_dict[setting]))
-    return conf_dict # Return the conf dict with the added configuration
+    # Return the conf dict with the added configuration
+    return conf_dict
+
 
 def get_file_contents(file_name, parent_dir=None):
-    """Reads a given file cleanly as bytes (to avoid decoding errors) and returns its contents. Raises errors if the file could not be read."""
+    """
+    Read a file as bytes (avoid decode errors) and returns its contents.
+
+    Raises errors if the file could not be read.
+    """
     # If the parent dir is not specified, use the home dir
-    if parent_dir == None: parent_dir = CONF["HomeDir"]
+    if parent_dir is None:
+        parent_dir = CONF["HomeDir"]
     # Treat the filename as a subpath of the parent
     file_path = os.path.join(parent_dir, file_name)
     # Cleanly open the file and return its contents
-    with open(file_path, "rb") as file: return file.read()
+    with open(file_path, "rb") as file:
+        return file.read()
 
 #
 # Server classes
@@ -188,17 +206,19 @@ def get_file_contents(file_name, parent_dir=None):
 
 
 class LanTalkServer(ThreadingMixIn, HTTPServer):
-    """The main server class which handles client connections and management"""
+    """The server class which handles client connections and management."""
 
     # Initialise class properties
-    signed_in_clients = {} # Dict of all clients with session IDs as keys
-    messages_to_send = [] # List of messages that are yet to be sent
-    threads = [] # A list of threads created by the server
-    run_threads = True # Variable controlling whether threads should be running
+    signed_in_clients = {}  # Dict of all clients with session IDs as keys
+    messages_to_send = []  # List of messages that are yet to be sent
+    threads = []  # A list of threads created by the server
+    run_threads = True  # Variable to control whether threads should be running
 
     # On object creation
     def __init__(self, bind_addr, request_handler):
-        # Run the initialisation function of the HTTPServer class (no need for `self` arg - it's passed automatically)
+        """Initialise server and starts threads."""
+        # Run the initialisation function of the HTTPServer class
+        # (no need for `self` arg - it's passed automatically)
         super().__init__(bind_addr, request_handler)
 
         # Save the request handler as a property (to allow exchanging data)
@@ -206,7 +226,8 @@ class LanTalkServer(ThreadingMixIn, HTTPServer):
 
         # Log that threads are being started
         log(0, "Starting threads")
-        # Start the threads (get all methods of the object and if their name starts with `thread_`, run them as threads)
+        # Start the threads (get all methods of the object and if their
+        # name starts with `thread_`, run them as threads)
         for thread in [getattr(self, method) for method in dir(self) if callable(getattr(self, method)) and method.startswith("thread_")]:
             # Add the thread to the list of threads
             self.threads.append(threading.Thread(target=thread))
@@ -216,25 +237,37 @@ class LanTalkServer(ThreadingMixIn, HTTPServer):
 
     # Client management methods
     def generate_session_id(self, username):
-        """Generates an ID for user sessions when they are logged in. The IDs follow this pattern: `<username>.<random number>`."""
+        """
+        Generate an ID for user sessions when they are logged in.
+
+        The IDs follow this pattern: `<username>.<random number>`.
+        """
         # Session IDs will consist of the username and a random ID
         return "{}.{}".format(username, "".join([str(random.randint(1,9)) for x in range(ID_SUFFIX_LENGTH)]))
 
     # Thread methods
     def thread_login_manager(self):
-        """A thread which manages user logins"""
+        """Manage user logins thread."""
         while self.run_threads:
-            pass # TODO: Create the thread
+            pass  # TODO: Create the thread
 
     def thread_disconnect_manager(self):
-        """A thread which constantly checks for the time of the last heartbeat of each user and removes any users which didn't show signs of life recently."""
+        """
+        Constantly check for the time of the last heartbeat of each user.
+
+        Removes any users which didn't show signs of life recently.
+        """
         while self.run_threads:
             for client in self.signed_in_clients:
-                pass # TODO: Create the thread
+                pass  # TODO: Create the thread
 
     # Misc. methods
     def stop_threads(self, wait_for_threads=True):
-        """Stop all threads started by this class and optionally wait for them to exit. Waits by default."""
+        """
+        Stop threads started by this class. Optionally wait for their exit.
+
+        Waits by default.
+        """
         # Log that the threads are being stopped
         log(0, "Stopping server threads")
         # Tell threads to stop
@@ -244,35 +277,37 @@ class LanTalkServer(ThreadingMixIn, HTTPServer):
             for thread in self.threads:
                 thread.join()
 
+
 class LanTalkServerRequestHandler(BaseHTTPRequestHandler):
-    """The main request handler class which processes requests made to the server"""
+    """Handle and processes requests made to the server."""
 
     # Modify BaseHTTPRequestHandler behavior
-    server_version = "LanTalkServer/{}".format(SOFTWARE_VERSION) # The "Server" header
-    sys_version = "" # Remove Python version from response (it's unnescessary and possibly a security problem)
-    protocol_version = "HTTP/1.1" # To support persistent connections for speed
+    server_version = "LTServer/{}".format(SOFTWARE_VERSION)  # "Server" header
+    sys_version = ""  # Remove Py version from response (unnescessary + possibly a security problem)
+    protocol_version = "HTTP/1.1"  # Support persistent connections for speed
 
     # Server methods
-    def log_message(self, form, *args): # Suppress default logging and only log if right log level set
-        """Logs important server events according to the global log level."""
+    def log_message(self, form, *args):  # Suppress default logging
+        """Log important server events according to the global log level."""
         # TODO: Fix this unholy creature and make it respect what's passed to it ^
         log(0, "{} request from {} for path {}".format(self.command, self.client_address[0], self.path))
 
     def do_GET(self):
-        """Runs when a GET request is received."""
-        self.respond("Nothing here yet!") # Temporary. GET requests will serve the panel at some point in the future (TODO)
+        """Run when a GET request is received."""
+        # Temporary. GET requests will serve the panel at some
+        # point in the future (TODO)
+        self.respond("Nothing here yet!")
 
-    def do_POST(self): # The chat protocol will use POST requests
-        """Runs when a POST requets is received."""
-        pass # TODO: Implement messaging protocol
+    def do_POST(self):  # The chat protocol will use POST requests
+        """Run when a POST requets is received."""
+        pass  # TODO: Implement messaging protocol
 
     # Misc. methods
     def respond(self, message):
-        """Send a response to the client with the message"""
-
+        """Send a response to the client with the message."""
         # Add headers
         self.send_response(200, "LanTalk Accepted Request")
-        self.send_header("Content-Length", len(message)) # Needed for persistent connections
+        self.send_header("Content-Length", len(message))  # For persistent conn
         self.end_headers()
 
         # Send the message
@@ -285,34 +320,39 @@ class LanTalkServerRequestHandler(BaseHTTPRequestHandler):
 
 
 def main():
-    """The main body of the LanTalk server script. Starts the server and runs any setup"""
-
+    """Run main body of LT server. Starts the server and runs setup."""
     # Define CONF as global as all parts of the script use it
     global CONF
 
-    # Define server as a global so that the request handler can access it's properties when requests are made
+    # Define server as a global so that the request handler
+    # can access it's properties when requests are made
     global server
 
-    try: # Exit cleanly no matter what
+    try:  # Exit cleanly no matter what
 
-        # Read the config in the script's dir, then put the config string through the config functions. The end result should be a valid config dict
+        # Read the config in the script's dir, then put the config string
+        # through the config functions. The end result should be a
+        # valid config dict
         CONF = conf_add_missing(conf_validate(conf_parse(get_file_contents(CONF_LOCATION, "").decode())))
 
-        CONF["LogLevel"] = 0 # Debugging override (to be commented out unless in development)
+        # Debugging override (to be commented out unless in development)
+        CONF["LogLevel"] = 0
 
         log(0, "Config read and parsed successfully")
 
         # Beginning
         log(1, "LanTalk Server starting")
-        time.sleep(0.5) # Wait a bit (it looks better :P)
+        time.sleep(0.5)  # Wait a bit (it looks better :P)
 
         # HTTP server section
         log(1, "Started listening on [{}:{}]".format(CONF["BindAddr"] if not CONF["BindAddr"] == "" else "*", CONF["BindPort"]))
         server = LanTalkServer((CONF["BindAddr"], int(CONF["BindPort"])), LanTalkServerRequestHandler)
-        try: server.serve_forever()
+        try:
+            server.serve_forever()
         except KeyboardInterrupt: pass
 
-        # When the server exits normally (for some reason) or there's a KeyboardInterrupt, do the following
+        # When the server exits normally (for some reason) or there's a
+        # KeyboardInterrupt, do the following
         log(1, "Stopped listening for connections")
 
         # Stop all threads started by the server
@@ -323,13 +363,17 @@ def main():
         # Clean exit
         sys.exit(0)
 
-    except Exception as err: # If there's any kind of error, log in and exit as cleanly as possible
+    # If there's any kind of error, log in and exit as cleanly as possible
+    except Exception as err:
         # Log the error first of all
         log(3, "Fatal error encountered. The server will exit cleanly.\nError: {}".format(err))
 
-        # Stop all threads since the script should have gotten far enough to start them but fail silently if this causes an error
-        try: server.stop_threads()
-        except: pass
+        # Stop all threads since the script should have gotten far enough to
+        # start them but fail silently if this causes an error
+        try:
+            server.stop_threads()
+        except:
+            pass
 
         # Exit cleanly but report error (non-zero exit code)
         sys.exit(1)
